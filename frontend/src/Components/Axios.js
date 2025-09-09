bef044b3093b1fbbf00f0bd8c00ae8f97d0ab098
radheshyam978 <vaishnavradhey978@gmail.com>
2025-09-09 16:05:56 +0530

    done

--- frontend/src/Components/Axios.js
+++ frontend/src/Components/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-baseURL: 'http://localhost:3500' || '/',
+baseURL: 'https://mobz-task-2-advance-1.onrender.com/' || '/',
 headers: { 'Content-Type': 'application/json' },
 });
 
--- frontend/src/Components/Socket.js
+++ frontend/src/Components/Socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 
-const SOCKET_URL = 'http://localhost:3500' || window.location.origin;
+const SOCKET_URL = 'https://mobz-task-2-advance-1.onrender.com' || window.location.origin;
 export const socket = io(SOCKET_URL, {
 autoConnect: false,
 });
